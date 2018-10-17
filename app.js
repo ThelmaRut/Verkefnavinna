224d9f2667be32685849c6ce949f0d91153425b4
Thelma Rut <thelmaj16@ru.is>
2018-10-17 16:42:58 +0000

    changing files in order to make the message in the server work

--- app.js
+++
@@ -1,8 +0,0 @@
-const app = require("./api");
-
-const PORT = process.env.PORT || 3000;
-
-
-app.listen(PORT, () => {
-    console.log("Server running on port" + PORT);
-});
---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const api = require("./api");
+
+// For all queries to localhost:3000/api/...
+// use the API router (see below)
+app.use("/api", api);
+
+// For any other route (URL) just send an error
+app.get("*", (req, res) => {
+  res.send({ error: "No route defined" });
+});
+
+module.exports = app;
